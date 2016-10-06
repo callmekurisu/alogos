5cc326a487b4ab8ae983d65e22c580db598a60f6
Nigel Christian <callmekurisu@gmail.com.unlinked>
2016-10-06 11:09:12 -1000

    Create uniqueArray.js

---
+++ uniqueArray.js
@@ -0,0 +1,32 @@
+
+function uniteUnique(...args) {
+  var arrUnique = [];
+  
+  for(i=0;i<args[0].length;i++){
+    var index1 = args[1].indexOf(args[0][i]);
+    
+    var index2 = args[2].indexOf(args[0][i]);
+     if (index1 > -1) {
+    args[1].splice(index1, 1);
+
+  }
+    if(args.length>2){
+      if (index2 > -1) {
+    args[2].splice(index2, 1);
+
+  }
+    }
+    
+  } 
+  
+  var flattened = args.reduce(function(a, b) {
+    return a.concat(b);
+}, []);
+  
+    
+  
+ return flattened;
+  
+}
+
+uniteUnique([1, 2, 3], [5, 2, 1]);
