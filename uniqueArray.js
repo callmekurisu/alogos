48cfc5419c10933f2998c547c28094d62f374ee7
Nigel Christian <callmekurisu@gmail.com.unlinked>
2016-10-08 09:39:39 -1000

    Update uniqueArray.js

--- uniqueArray.js
+++ uniqueArray.js
@@ -1,8 +1,25 @@
-
 function uniteUnique(...args) {
   var arrUnique = [];
   
-  for(i=0;i<args[0].length;i++){
+  if(args.length<=2){
+    
+      for(i=0;i<args[0].length;i++){
+    var index1 = args[1].indexOf(args[0][i]);
+    
+     if (index1 > -1) {
+    args[1].splice(index1, 1);
+
+  }
+  
+    
+  } 
+  
+    
+  }
+  
+  if(args.length>2){
+    
+      for(i=0;i<args[0].length;i++){
     var index1 = args[1].indexOf(args[0][i]);
     
     var index2 = args[2].indexOf(args[0][i]);
@@ -19,6 +36,9 @@ function uniteUnique(...args) {
     
   } 
   
+    
+  }
+
   var flattened = args.reduce(function(a, b) {
     return a.concat(b);
 }, []);
@@ -28,5 +48,6 @@ function uniteUnique(...args) {
  return flattened;
   
 }
+
 
 uniteUnique([1, 2, 3], [5, 2, 1]);
